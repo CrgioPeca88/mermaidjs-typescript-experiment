c56eb8f7d5380ae225780f339741ee2d6eb75a65
CrgioPeca88 <crgiopeca88@gmail.com>
2021-05-06 13:02:38 -0500

    [FEATURE] commit with flow diagram from Js

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,41 @@ import mermaid from "mermaid";
 
 mermaid.initialize({
     startOnLoad: true,
-    theme: 'dark'
+    theme: 'forest',
+    flowchart: {
+        useMaxWidth: true,
+        diagramPadding: 15,
+        nodeSpacing: 20
+    }
 });
+
+const mermaidDiagram = document.getElementById('mermaid-diagram');
+mermaidDiagram.innerHTML = `
+    graph TB
+        A((Nodo A))-->B((Nodo B))
+        B-->C((Nodo C))
+        C-->D((Nodo D))
+        D-->E((Nodo E))
+        E-->F((Nodo F))
+        F-->G((Nodo G))
+        G-->H((Nodo H))
+        A-->C
+        A-->D
+        A-->E
+        A-->F
+        A-->G
+        A-->H
+        A-->I((Nodo I))
+        A-->J((Nodo J))
+        A-->K((Nodo K))
+        A-->L((Nodo L))
+        A-->M((Nodo M))
+        A-->N((Nodo N))
+        A-->Ñ((Nodo Ñ))
+        A-->O((Nodo O))
+        A-->P((Nodo P))
+        A-->Q((Nodo Q))
+        A-->R((Nodo R))
+        A-->S((Nodo S))
+        A-->T((Nodo T))
+`;
