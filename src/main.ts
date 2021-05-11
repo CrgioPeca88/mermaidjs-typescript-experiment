b5adc0cbd200b0177472b483e9ff4d3ad4b13786
CrgioPeca88 <crgiopeca88@gmail.com>
2021-05-11 13:19:55 -0500

    [Feature] clear button and add node buttons added

--- src/main.ts
+++ src/main.ts
@@ -1,81 +1,80 @@
 import mermaid from "mermaid";
 
+/*type NodeType = 'parent' | 'child'
+
+interface NodeChild {
+    name: string;
+}
+
+interface Node {
+    id: string
+    name: string;
+    type: NodeType;
+    roads: NodeChild[]
+}*/
+
 let mermaidDiagram: HTMLElement;
-let startButton: HTMLElement;
-let graph2: HTMLElement;
+let clear: HTMLElement;
 let addNode: HTMLElement;
 let count: number;
-let testDiagram: string;
+let startDiagram: string;
+let tree: string;
 
 (function () {
-    count = 2;
-    testDiagram = `
+    count = 1;
+    startDiagram = `
         graph TB
-            subgraph Padres
+            subgraph vertical[Padres]
                 1((Nodo 1))
             end
-            subgraph Hijos
-                1-->2((Nodo 2))
-            end
     `;
+    tree = startDiagram;
     mermaidDiagram = document.getElementById('mermaid-diagram');
-    startButton = document.getElementById('start');
-    graph2 = document.getElementById('graph2');
     addNode = document.getElementById('addNode');
+    clear = document.getElementById('clear');
+    mermaidDiagram.innerHTML = tree;
     mermaid.initialize({
-        startOnLoad: false,
+        startOnLoad: true,
         theme: 'default',
         flowchart: {
-            useMaxWidth: false,
+            useMaxWidth: true,
             diagramPadding: 15,
             nodeSpacing: 20
         }
     });
 })()
 
-function getDiagram(): string {
-    return `
-    graph TB
-        subgraph Zona1
-            A((Nodo A))
-        end
-        subgraph Zona2
-            A-->B((Nodo B))
-            B-->C((Nodo C))
-            C-->D((Nodo D))
-            D-->E((Nodo E))
-            E-->F((Nodo F))
-            F-->G((Nodo G))
-            G-->H((Nodo H))
-            A-->C
-            A-->D
-            A-->E
-            A-->F
-            A-->G
-            A-->H
-            A-->I((Nodo I))
-            A-->J((Nodo J))
-            A-->K((Nodo K))
-            A-->L((Nodo L))
-            A-->M((Nodo M))
-            A-->N((Nodo N))
-            A-->Ñ((Nodo Ñ))
-            A-->O((Nodo O))
-            A-->P((Nodo P))
-            A-->Q((Nodo Q))
-            A-->R((Nodo R))
-            A-->S((Nodo S))
-            A-->T((Nodo T))
-        end
-    `;
+function increaseCounter(n: number): void {
+    count = count + n;
 }
 
-function getGrafo(): string {
-    return testDiagram;
+function clearCounter(n: number): void {
+    count = n;
 }
 
-function setGrafo(newNode: String): void {
-    testDiagram =`${testDiagram}\n${newNode}`;
+function getCounter(): number {
+    return count;
+}
+
+function getTree(): string {
+    return tree;
+}
+
+function clearTree(): void {
+    tree = startDiagram;
+}
+
+function AddNodeToTree(newNode: String): void {
+    if (getCounter() > 1) {
+        tree =`
+            ${tree}\n
+            subgraph childs[Hijos]
+            ${newNode}
+            end
+        `;
+    } else {
+        tree.replace(`end`, `${newNode}\n end`);
+    }
 }
 
 function renderDiagram(diagram: string): void {
@@ -84,16 +83,17 @@ function renderDiagram(diagram: string): void {
     });
 }
 
-startButton.onclick = () => {
-    renderDiagram(getDiagram());
-}
-
-graph2.onclick = () => {
-    renderDiagram(getGrafo());
-}
-
 addNode.onclick = () => {
-    count++;
-    setGrafo(`1-->${count}((Nodo ${count}))`);
-    renderDiagram(getGrafo());
+    const from = prompt('Seleccione nodo PADRE:')
+    if (from) {
+        increaseCounter(1)
+        AddNodeToTree(`${from}-->${count}((Nodo ${count}))`);
+        renderDiagram(getTree());
+    }
+}
+
+clear.onclick = () => {
+    clearTree();
+    clearCounter(1);
+    renderDiagram(getTree());
 }
